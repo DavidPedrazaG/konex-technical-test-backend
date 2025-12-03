e5acb4013e8ceaa78fc9473905fb4ea8baa85260
DavidPedrazaG <davidpipepedraza@gmail.com>
2025-12-03 02:27:29 -0500

    🚧 [ADD] Init project

---
+++ src/main/java/konex_technical_test_backend/KonexTechnicalTestBackendApplication.java
@@ -0,0 +1,13 @@
+package konex_technical_test_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KonexTechnicalTestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KonexTechnicalTestBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/konex_technical_test_backend/KonexTechnicalTestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package konex_technical_test_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonexTechnicalTestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
